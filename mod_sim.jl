212a0bcc57f11ed6c5b5a2bffaa3807a18afdad3
jackhardy <60779248+jjskr@users.noreply.github.com>
2023-03-22 15:03:02 +0000

    bugs fixed

--- mod_sim.jl
+++ mod_sim.jl
@@ -14,20 +14,24 @@ struct SimAnnealing
 end
 
 # Initialising struct
-sim = SimAnnealing([step1, step2, step3, step4, step5, step6, step8, step9], objective, cool_fun, metro_fun, 5, 2, 10, 50000)
+sim = SimAnnealing([step1, step2, step3, step4, step5, step6, step8, step9], objective, cool_fun, metro_fun, 10, 3, 10, 100000)
 # opt = gen_in(35146671702464, 10)
 
 # Initialising values and step count list
 cur, cur_eval, steps_taken = initialise(sim.population, sim.restrict)
 
 # Simulated Annealing
-cur, step_count = sim_ann(sim.it_tot, sim.population, sim.steptype, sim.obj_function, sim.restrict, sim.cooling, sim.init_temp, cur, cur_eval, sim.metropolis, steps_taken)
-
-sum(cur)
-show(stdout, "text/plain", cur)
-objective(cur, sim.population, sim.restrict)
-mem_calcs_full(cur, sim.population)
-steps_taken
-println(cur_eval)
+new, new_eval, step_count = sim_ann(sim.it_tot, sim.population, sim.steptype, sim.obj_function, sim.restrict, sim.cooling, sim.init_temp, cur, cur_eval, sim.metropolis, steps_taken)
+sum(step_count)
+sum(new)
+show(stdout, "text/plain", new)
+objective(new, sim.population, sim.restrict)
+mem_calcs_full(new, sim.population)
+println(new_eval)
 
 godd = copy(cur)
+
+ok1 = gen_in(3000002, 10)
+mem_calcs_full(ok1, 10)
+ok2 = gen_in(3000001, 10)
+mem_calcs_full(ok2, 10)
